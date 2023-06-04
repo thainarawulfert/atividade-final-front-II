6a95f43704fbeeacbe56a36eaf992f761a6f6609
Thainara Wulfert <thainarawulfertoliveira@gmail.com>
2023-06-04 20:02:45 -0300

    chamei a API

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+window.onload = () => [loadPagina()];
+
 const urlPrincipal = axios.create({
   baseURL: "https://rickandmortyapi.com/api",
 });
 
-function loadPagina() {}
+function loadPagina(url) {
+  urlPrincipal.get(url).then((res) => {
+    const paginas = res.data;
+    console.log(paginas);
+  });
+}
